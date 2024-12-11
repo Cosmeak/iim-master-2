b1f8d54d70a286574f0b1ff840329a5ec733068e
Guillaume Fine <90453342+Cosmeak@users.noreply.github.com>
2024-12-11 14:03:39 +0100

    wip: circuit

--- ftd_client/ftd_client.ino
+++ ftd_client/ftd_client.ino
@@ -1,29 +1,43 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
+#include <Servo.h>
 
+// Board pins
 const int bpin = 5;  // GPIO5 (D1)
-WiFiUDP Client;
+Servo door;
 
+// Communication
+WiFiUDP Client;
 bool isFirstMessage = true; // Used for our identification protocol on the server side
 uint8_t selfIndex = 1; // Identifier on the server
+char packetBuffer[255];
+
+// Others
+bool ballCanGo = true; // Used to know if we can launch the circuit
 
 void setup() {
   Serial.begin(115200);            
   pinMode(bpin, INPUT_PULLUP);
+  door.attach(4); // GPIO4 (D2)
 }
 
 void loop() {
   // Connect to server hotspot if not connected to it
-  if (WiFi.isConnected()) {
-    connectToWifi();
-  }
+  // if (!WiFi.isConnected()) {
+  //   connectToWifi();
+  // }
 
   // Check if packet is received and parse it if exists
-  int packetSize = udp.parsePacket();
+  int packetSize = Client.parsePacket();
   if (packetSize) {
     Serial.println("A message is received.");
-    int len = udp.read(packetBuffer, 255);
-    if (len > 0) packetBuffer[len - 1] = 0;
+    int len = Client.read(packetBuffer, 255);
+    if (len > 0) {
+      // Circuit can be launched
+      if (packetBuffer[0] == 1 && !ballCanGo) ballCanGo = true;
+      // An error occured
+      if (packetBuffer[0] == 0) { } // TODO: handle error 
+    };
     Serial.print(packetBuffer);
     Serial.println("\n");   
   }
@@ -31,12 +45,25 @@ void loop() {
   // Read button state and send message to server
   int bstate = digitalRead(bpin); 
   if (bstate == LOW && isFirstMessage) {
-    sendMessage((char*) "message");
-  } else if (bstate == LOW && !isFirstMessage) {
     sendMessage((char*) &selfIndex);
+  } else if (bstate == LOW && !isFirstMessage && ballCanGo) {
+    sendMessage((char*) 1);
   }
 
-  delay(100);
+  // Handle circuit events
+  if (ballCanGo) {
+    // Open the door and close it before detach
+    door.write(8);
+    delay(800);
+    door.write(180);
+    delay(800);
+    door.detach();
+
+    // Wait for ultrasonic sensor to detect the ball
+    // TODO
+  }
+
+  delay(5000);
 }
 
 void connectToWifi() {
@@ -70,4 +97,8 @@ void sendMessage(char *message) {
   Client.write(message);
   Client.write("\r\n");
   Client.endPacket();
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  return (microseconds / 2) / 29.1 ;
 }
