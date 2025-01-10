32da536dd423e6401922b13e2ad3f991eb302a4e
Guillaume Fine <90453342+Cosmeak@users.noreply.github.com>
2025-01-10 09:11:51 +0100

    Initial Commit

---
+++ fine_mzali/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fine_mzali
+//
+//  Created by Guillaume Fine on 10/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fine_mzali/fine_mzaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  fine_mzaliApp.swift
+//  fine_mzali
+//
+//  Created by Guillaume Fine on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct fine_mzaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
